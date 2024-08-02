d64bce66910153df5865121d0770afa9d6e7c945
JD <john.dobson@fodmobilitygroup.com>
2024-08-02 15:18:37 +0100

    New route for get all. Forgot a request

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CompleteTaskController;
 use App\Http\Controllers\CreateTaskController;
 use App\Http\Controllers\DeleteTaskController;
+use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,6 +21,7 @@ Route::get('/', function () {
     return view('tasks');
 });
 
+Route::get('/tasks', TasksController::class)->name('tasks');
 Route::post('/create', CreateTaskController::class)->name('create.task');
-Route::delete('/delete/{task}', DeleteTaskController::class)->name('delete.task');
+Route::post('/delete/{task}', DeleteTaskController::class)->name('delete.task');
 Route::post('/complete/{task}', CompleteTaskController::class)->name('complete.task');
